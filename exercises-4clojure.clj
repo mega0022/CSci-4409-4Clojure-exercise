cb96cc1a8f5c195c126a2aba0e4bf28231175628
Henry Megarry <mega0022@morris.umn.edu>
2015-09-07 18:52:34 -0500

    answered the questions

--- exercises-4clojure.clj
+++ exercises-4clojure.clj
@@ -33,3 +33,121 @@
 
 (= (#(* 2 %) 11) 22)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 10: Intro to Maps
+
+;What I pasted in the box:
+20
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 16: Hello World
+
+;What I pasted in the box:
+(fn [n] (str "Hello, " n "!"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 17: Sequences: map
+
+;What I pasted in the box:
+'(6 7 8)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 18: Sequences: filter
+
+;What I pasted in the box:
+'(6 7)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 20 - Penultimate Element
+
+;What I pasted in the box:
+#(nth %1 (- (count %1) 2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 21 - Nth Element
+
+;What I pasted in the box:
+(fn myNth[n m] (if (> m 0) (myNth(rest n) (- m 1))(first n)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 23 - Reverse a Sequence
+
+;What I pasted in the box:
+(fn myReverse[n] (if (= (peek (vec n)) nil) [] (conj (myReverse (subvec (vec n) 1 (count (vec n)))) (get (vec n) 0) )))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 25 - Find the odd numbers
+
+;What I pasted in the box:
+filter odd?
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 37 - Regular Expressions
+
+;What I pasted in the box:
+"ABC"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 40 - Interpose a Seq
+
+;What I pasted in the box:
+(fn myInterpose[n m] (if (> (count m) 1) (concat (vector (get m 0) x) (myInterpose n (subvec m 1 (count m)))) (vector (get m 0))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 43 - Reverse Interleave
+
+;What I pasted in the box:
+(fn reverseInterLeave[m, p] (reverse ((fn reverseInterLeaveIt [n, f] 
+      (if (> (count n) 1) 
+        (reverseInterLeaveIt (first (rest n)) (conj f (first n))) 
+        (conj f (first n)))) ((fn main[x, y & [c]]
+  (if ( = c y)
+  nil
+  (remove nil? (list   
+   ((fn helper[l, c] 
+      (if (< c (count l)) 
+         (concat (list (nth l c)) (helper l (+ c y))) 
+         nil)) x 0 ) 
+   (main (rest x) y (if c (+ c 1) 1)) ) ) )) m p) '())))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 44 - Rotate Sequence
+
+;What I pasted in the box:
+
+(fn rotate[x, y] (
+                  if (< x 0)
+                    (rotate (+ x 1) (concat (list (last y)) (subvec (vec y) 0 (- (count y) 1))))
+                    (if (> x 0)
+                      (rotate (- x 1) (concat (rest y) (list (first y))))
+                      y
+                    )
+                  )
+                  
+                  
+                  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 46 - Flipping out
+
+;What I pasted in the box:
+  (fn [f] (fn [n m] (f m n)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Problem 47 - Contain yourself
+
+;What I pasted in the box:
+4
